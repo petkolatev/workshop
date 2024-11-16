aea53edfc2f8b9331dd9dbd5318382ca5b60ba8d
petkolatev <petkorlatev@gmail.com>
2024-11-16 18:20:16 +0200

    finish workshop1

--- src/app/posts-list/posts-list.component.ts
+++ src/app/posts-list/posts-list.component.ts
@@ -1,20 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../api.service';
+import { Post } from '../types/post';
+import { LoaderComponent } from "../shared/loader/loader.component";
 
 @Component({
   selector: 'app-posts-list',
   standalone: true,
-  imports: [],
+  imports: [LoaderComponent],
   templateUrl: './posts-list.component.html',
   styleUrl: './posts-list.component.css'
 })
 export class PostsListComponent implements OnInit {
+  posts:Post[] = []
+  isLoading = true
   constructor(private apiService: ApiService) {
 
   }
   ngOnInit(): void {
-    this.apiService.getPosts().subscribe(p => {
-      console.log(p);
+    this.apiService.getPosts(5).subscribe((posts) => {
+      console.log(posts);
+      this.posts = posts
+      this.isLoading = false
 
     })
   }
--- src/app/themes-list/themes-list.component.ts
+++ src/app/themes-list/themes-list.component.ts
@@ -1,21 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../api.service';
+import { Theme } from '../types/theme';
+import { LoaderComponent } from '../shared/loader/loader.component';
 
 @Component({
   selector: 'app-themes-list',
   standalone: true,
-  imports: [],
+  imports: [LoaderComponent],
   templateUrl: './themes-list.component.html',
   styleUrl: './themes-list.component.css'
 })
 export class ThemesListComponent implements OnInit {
+  themes: Theme[] = []
+  isLoading = true
+
   constructor(private apiService: ApiService) {
 
   }
 
   ngOnInit(): void {
-    this.apiService.getThemes().subscribe(t => {
-      console.log(t);
+    this.apiService.getThemes().subscribe((themes) => {
+      this.themes = themes
+      this.isLoading = false
 
     })
   }
